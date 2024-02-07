d98a7ecc00214e7554be6bf55606d270f294bf70
Kaden <kaden.larussa@selu.edu>
2024-02-07 11:54:25 -0600

    added string push

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 class Stack {
 public:
-    char s[20];
+    char charStack[100];
     int top;
     void init() {
         top = -1;
@@ -16,17 +16,30 @@ public:
     }
     void push(char c) {
         top++;
-        s[top] = c;
+        charStack[top] = c;
+    }
+    void pushString(const std::string &word) {
+        for (int i = 0; i < word.length(); i++) {
+                push(word[i]);
+        }
+    }
+    std::string stringPop() {
 
     }
+
     char pop() {
-        char c = s[top];
+        char c = charStack[top];
         top--;
         return c;
     }
     void showStack() {
-        for (int i = 0; i <= top; i++) {
-            std::cout << s[i] << " ";
+        if(!isStackEmpty()) {
+            for (int i = 0; i <= top; i++) {
+                std::cout << charStack[i] << " ";
+            }
+        }
+        else {
+            std::cout << "Stack is empty!" << std::endl;
         }
     }
 
@@ -45,6 +58,25 @@ public:
 };
 
 int main() {
-Stack stack;
-    stack.testCase("TestString");
+    malloc(20);
+    std::cout << "Start" << std::endl;
+    Stack stack;
+    stack.init();
+    std::cout << "2" << std::endl;
+    std::cout << std::endl;
+    stack.pushString("bob");
+    stack.showStack();
+    stack.pop
+    stack.pushString("aet normel");
+    stack.showStack();
+    std::cout << std::endl;
+    std::cout << std::endl;
+    stack.pushString("snoino dna revil etah");
+    stack.showStack();
+    std::cout << std::endl;
+    std::cout << std::endl;
+    stack.pushString("SELUR LOBOC");
+
+    stack.showStack();
+    return 0;
 }
